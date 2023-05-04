c36e8030553be8443f780b5a8290caed3470ced5
Melanie Wilson <melaniemwilson1@gmail.com>
2023-05-03 20:50:22 -0400

    start building out handleSubmit

--- script.js
+++ script.js
@@ -13,8 +13,22 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
     // getAllRestaurants()
-    // document.querySelector('.add-restaurant-form').addEventListener('submit', handleSubmit)
+    document.querySelector('.add-restaurant-form').addEventListener('submit', handleSubmit)
   });
+
+  function handleSubmit(e){
+    e.preventDefault()
+    let restaurantObj = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      neighborhood: e.target.neighborhood.value,
+    //   rating: e.target.rating.value,
+      comments: e.target.comments.value
+    }
+    console.log(restaurantObj)
+    // renderOneToy(restaurantObj)
+    // postToy(restaurantObj)
+  }
 
 //   function getAllRestaurants(){
 //     fetch("http://localhost:3000/toys")
